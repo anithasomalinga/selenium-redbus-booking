aa6cc11662b0922647f684cbb54c2bed9c035830
anithasomalinga <ani2485@gmail.com>
2026-03-14 15:59:47 -0700

    RedBus - search and display bus names

--- src/main/java/org/anitha/RedBusBooking.java
+++ src/main/java/org/anitha/RedBusBooking.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RedBusBooking {
@@ -42,7 +43,7 @@ public class RedBusBooking {
         List<WebElement> searchCategories = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(searchCategoryLocator, 1));
 
         WebElement locationSelection = searchCategories.get(0);
-        By listHeaderLocator = By.xpath("//div[contains(@class,\"listHeader\")]");
+        By listHeaderLocator = By.xpath(".//div[contains(@class,\"listHeader\")]");
         List<WebElement> locationList = locationSelection.findElements(listHeaderLocator);
 
         for(WebElement webElement : locationList) {
@@ -76,9 +77,42 @@ public class RedBusBooking {
         WebElement dateOptionTomorrow = dateOptions.get(1);
         dateOptionTomorrow.click();
 
-        By searchButtonLocator = By.xpath("//button[contains(@class,\"primaryButton\")]");
-        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(searchButtonLocator));
-        searchButton.click();
+//        By searchButtonLocator = By.xpath("//button[contains(@class,\"primaryButton\")]");
+//        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(searchButtonLocator));
+//        searchButton.click();
+
+        By primoBusLocator = By.xpath("//div[contains(text(),\"Primo Bus\")]");
+        wait.until(ExpectedConditions.elementToBeClickable(primoBusLocator)).click();
+
+        By acLocator = By.xpath("//div[contains(text(),\"AC\")]");
+        wait.until(ExpectedConditions.elementToBeClickable(acLocator)).click();
+
+        By noOfBusesLocator = By.xpath("//span[contains(@class,\"subtitle\")]");
+        WebElement noOfBuses;
+        if(wait.until(ExpectedConditions.textToBePresentInElementLocated(noOfBusesLocator, "buses"))) {
+            noOfBuses = driver.findElement(noOfBusesLocator);
+            System.out.println(noOfBuses.getText());
+        }
+
+        By busesFrameLocator = By.xpath("//li[contains(@class,\"tupleWrapper\")]");
+        By busNameLocator = By.xpath(".//div[contains(@class,\"travelsName\")]");
+        By endOfListLocator = By.xpath("//span[contains(@class,\"endText\")]");
+
+        while(true) {
+            List<WebElement> rowList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(busesFrameLocator));
+            List<WebElement> endOfList = driver.findElements(endOfListLocator);
+            if(!endOfList.isEmpty()) {
+                break;
+            }
+            js.executeScript("arguments[0].scrollIntoView({behavior:'smooth'})", rowList.get(rowList.size() - 3));
+        }
+        List<WebElement> busList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(busesFrameLocator));
+        System.out.println("No of buses: " + busList.size());
+        for(WebElement element : busList) {
+            WebElement travelName = element.findElement(busNameLocator);
+            System.out.println(travelName.getText());
+        }
+
 
     }
 }
